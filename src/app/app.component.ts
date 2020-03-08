ecaacdbd42b13094605df742aa2c5c56748d66b7
Sergio Lopes <sergiooliveiralopes@yahoo.com.br>
2020-03-08 19:06:06 -0300

    Agora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Alurapic'
+  photos = [
+    {
+    url: 'https://thumbs.dreamstime.com/b/leao-beach-fernando-de-noronha-island-lion-bay-waters-atlantic-ocean-archipelag-pernambuco-brazil-45395949.jpg',
+    description: 'Imagem 1'
+    },
+    {
+    url: 'https://thumbs.dreamstime.com/b/beautiful-view-praia-de-leao-lion-beach-fernando-de-noronha-brazil-state-pernambuco-brazil-view-praia-de-leao-145792800.jpg',
+    description: 'Imagem 2'
+    }
+    ];
  }
